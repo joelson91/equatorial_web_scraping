13a88aca2e64707eb5a1a37d0d3a1ff095ac4641
joelson <joelsonandradep@gmail.com>
2025-09-16 19:27:58 -0300

    fix: fix chrome import options

--- src/script.py
+++ src/script.py
@@ -2,7 +2,7 @@
 import os
 import logging
 from selenium import webdriver
-from selenium.webdriver.chrome import Options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
